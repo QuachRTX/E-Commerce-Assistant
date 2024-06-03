d8b2b74133755ec0c6b1f37e9bea73545d8883e3
Alexsander Stahnke <93165922+QuachRTX@users.noreply.github.com>
2024-06-03 17:31:03 -0300

    Update checkresultord.py

--- checkresultord.py
+++ checkresultord.py
@@ -1,7 +1,7 @@
 __author__ = "Alexsander Stahnke"
 __version__ = "1.42"
 __status__ = "Stable"
-__email__ = "alexsander.stahnke@grendene.com.br"
+__email__ = "quach.vrc@gmail.com"
 __github__ = "https://github.com/QuachRTX"
 
 from tkinter import filedialog
